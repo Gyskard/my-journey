40fc9c47a83c4cf9bcdaae575637c226d362f196
Thomas Margueritat <thomas@marguerit.at>
2022-01-30 12:12:52 +0100

    improved locationForm service

--- front/src/services/locationFormatService.js
+++ front/src/services/locationFormatService.js
@@ -3,10 +3,16 @@ let locationFormatService = {};
 locationFormatService.install = function (Vue) {
     Vue.prototype.$locationFormat = function (location) {
         let result = location["name"]
+        location = Object.assign({}, {
+            "house_number_street": location["house_number_street"],
+            "street_name": location["street_name"],
+            "postal_code": location["postal_code"],
+            "city": location["city"],
+            "country": location["country"],
+        })
         if (Object.values(location).some(elm => elm !== null)) {
             result += " ("
             for (const elm of ["house_number_street", "street_name", "postal_code", "city", "country"]) {
-
                 result += `${location[elm] !== null ? location[elm] + ", " : ""}`
             }
             result = result.slice(0,-2) + ")"
