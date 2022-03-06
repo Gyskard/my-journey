6d8d28ddddad2ae553c892d293b2a29e9ec4077d
Thomas Margueritat <thomas@marguerit.at>
2022-03-06 10:50:40 +0100

    Added pages system for List component

--- front/src/components/List.vue
+++ front/src/components/List.vue
@@ -1,8 +1,13 @@
 <template>
   <v-container>
-    <v-expansion-panels focusable style="max-width: 75em;">
-      <Event v-for="event in events" :key="event" :event="event" class="mt-3"/>
-    </v-expansion-panels>
+    <div>
+      <v-expansion-panels focusable class="mx-3" style="max-width: 75em">
+        <Event v-for="event in events" :key="event" :event="event" />
+      </v-expansion-panels>
+      <div v-if="eventsPageNumber !== 0" class="text-center mt-7">
+        <v-pagination v-model="selectedPage" :length="eventsPageNumber" class="d-block"></v-pagination>
+      </div>
+    </div>
   </v-container>
 </template>
 
@@ -19,7 +24,10 @@ export default {
   props: ["searchForm"],
 
   data: () => ({
-    events: []
+    events: [],
+    eventsPageNumber: 1,
+    selectedPage: null,
+    oldSelectedPage: null
   }),
 
   created() {
@@ -27,14 +35,17 @@ export default {
       search: null,
       date: null,
       order_by: "Descending",
-    })
+    }, 0)
   },
 
   methods: {
-    getEvents: function (search) {
+    getEvents: function (search, selectedPage) {
+      search.offset = selectedPage
       this.$http.post(this.$api + "/event/filter", search)
         .then(response => {
-          this.events = response.data
+          this.events = response.data["events"]
+          this.eventsPageNumber = response.data["events_page_number"]
+          this.selectedPage = selectedPage + 1
         })
         .catch(error => {
           this.$emit('display', {
@@ -49,9 +60,19 @@ export default {
   watch: {
     searchForm: {
       handler(search) {
-        this.getEvents(search)
+        this.getEvents(search, 0)
       },
       deep: true
+    },
+    selectedPage: function (value) {
+      if (this.oldSelectedPage !== null) {
+        if (value !== undefined) {
+          let form = this.searchForm
+          if (form.order_by === undefined) form.order_by = "Descending"
+          this.getEvents(form, value - 1)
+        }
+      }
+      this.oldSelectedPage = value
     }
   }
 }
